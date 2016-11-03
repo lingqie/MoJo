839d48d9053ed6be16c71ef9300d2b4fc6991298
Yabou <1193799117@qq.com>
2016-11-03 16:36:30 +0800

    just lost something to push

---
+++ src/main/java/mojong/MoJoPaiCode.java
@@ -0,0 +1,52 @@
+package mojong;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoJoPaiCode {
+	static final int WAN = 0;
+	static final int WAN1 = 0;
+	static final int WAN2 = 1;
+	static final int WAN3 = 2;
+	static final int WAN4 = 3;
+	static final int WAN5 = 4;
+	static final int WAN6 = 5;
+	static final int WAN7 = 6;
+	static final int WAN8 = 7;
+	static final int WAN9 = 8;
+	static final int BIN = 9;
+	static final int BIN1 = 9;
+	static final int BIN2 = 10;
+	static final int BIN3 = 11;
+	static final int BIN4 = 12;
+	static final int BIN5 = 13;
+	static final int BIN6 = 14;
+	static final int BIN7 = 15;
+	static final int BIN8 = 16;
+	static final int BIN9 = 17;
+	static final int SUO = 18;
+	static final int SUO1 = 18;
+	static final int SUO2 = 19;
+	static final int SUO3 = 20;
+	static final int SUO4 = 21;
+	static final int SUO5 = 22;
+	static final int SUO6 = 23;
+	static final int SUO7 = 24;
+	static final int SUO8 = 25;
+	static final int SUO9 = 26;
+	static final int DONG = 27;// 东
+	static final int NAN = 28;
+	static final int XI = 29;
+	static final int BEI = 30;
+	static final int BAI = 31;// 白
+	static final int FA = 32;// 发
+	static final int ZHONG = 33;// 中
+	static final int [] YAOJIULIST ={0,8,9,17,18,26,27,28,29,30,31,32,33};
+	static final int [] FENGPAILIST ={27,28,29,30};
+	static final String [] PAILIST={
+			"1m","2m","3m","4m","5m","6m","7m","8m","9m",
+			"1p","2p","3p","4p","5p","6p","7p","8p","9p",
+			"1s","2s","3s","4s","5s","6s","7s","8s","9s",
+			"1z","2z","3z","4z","5z","6z","7z"
+			};
+}
